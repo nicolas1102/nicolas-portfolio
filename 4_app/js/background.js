f3685ec15d83efc54503dc586a949d77ee24db6e
adrianadiazv <nicolasdiazv1102@gmail.com>
2023-05-09 17:30:44 -0500

    styling mobile view

---
+++ 4_app/js/background.js
@@ -0,0 +1,5 @@
+const background = document.querySelector('#background-pattern');
+const main = document.querySelector('main');
+
+// calculate for background
+background.style.height = main.offsetHeight + 1 + 'px';
